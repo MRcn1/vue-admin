7f891d17c57d20c41e84bb62c7de6f0bcb01dd25
zenghuifan <3558589063@qq.com>
2018-09-17 21:18:54 +0800

    9.17 21：18 zhf

--- src/api/admin2.js
+++ src/api/admin2.js
@@ -2,3 +2,7 @@ import {fetch,post} from '../config/http.js'
 export function merchants(params){
     return fetch("/shopping/restaurants",{...params})
 }
+
+export function merchants_post(params){
+    return post("/shopping/restaurants",{...params})
+}
